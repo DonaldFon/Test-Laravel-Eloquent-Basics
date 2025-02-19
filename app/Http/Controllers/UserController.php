ed9bf22876df68aa2489912f627c3f602ce785cf
Donald <154738239+DonaldFon@users.noreply.github.com>
2025-02-20 00:52:44 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,7 +47,7 @@ class UserController extends Controller
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
         $user=User::where('name',$name)->first();
-        if($user){
+        if(!empty($user)){
             $user->update([
                           'email'=>$email
             ]);
